cf3aa40dd4202c30443a2af3392c8d14f6b3930f
Vladimir Hiuk <khiukv@gmail.com>
2020-09-05 10:19:48 +0200

    Finish redux and Table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,15 +1,18 @@
-import {DomListener} from './DomListener';
+import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.subscribe = options.subscribe || [];
+    this.store = options.store;
     this.unsubscribers = [];
 
     this.prepare();
   }
 
+  // Configuring our component before init
   prepare() {}
 
   // Returns the component template
@@ -17,21 +20,38 @@ export class ExcelComponent extends DomListener {
     return '';
   }
 
+  // Notifying listeners about the event
   $emit(event, ...args) {
     this.emitter.emit(event, ...args);
   }
 
+  // Subscribe to event
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
   }
 
-  init() {
-    this.initDOMListener();
+  $dispatch(action) {
+    this.store.dispatch(action);
   }
 
+  // Only changes in the fields that we subscribed to come here
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key);
+  }
+
+  // Initializing the component
+  // Adding DOM listeners
+  init() {
+    this.initDOMListeners();
+  }
+
+  // Removing the component
+  // Clean the listeners
   destroy() {
-    this.removeDOMListener();
+    this.removeDOMListeners();
     this.unsubscribers.forEach(unsub => unsub());
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -14,7 +14,7 @@ class Dom {
   }
 
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text;
       return this;
     }
@@ -35,6 +35,10 @@ class Dom {
 
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback);
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector));
   }
 
   append(node) {
@@ -63,18 +67,23 @@ class Dom {
     return this.$el.getBoundingClientRect();
   }
 
-  find(selector) {
-    return $(this.$el.querySelector(selector));
-  }
-
   findAll(selector) {
     return this.$el.querySelectorAll(selector);
   }
 
   css(styles = {}) {
-    Object.keys(styles).forEach(key => {
-      this.$el.style[key] = styles[key];
-    });
+    Object
+        .keys(styles)
+        .forEach(key => {
+          this.$el.style[key] = styles[key];
+        });
+  }
+
+  getStyles(styles = []) {
+    return styles.reduce((res, s) => {
+      res[s] = this.$el.style[s];
+      return res;
+    }, {});
   }
 
   id(parse) {
@@ -93,6 +102,14 @@ class Dom {
     return this;
   }
 
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value);
+      return this;
+    }
+    return this.$el.getAttribute(name);
+  }
+
   addClass(className) {
     this.$el.classList.add(className);
     return this;
@@ -104,7 +121,6 @@ class Dom {
   }
 }
 
-// event.target
 export function $(selector) {
   return new Dom(selector);
 }
